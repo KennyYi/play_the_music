b09f2072eff04b7f7d5e57d943f931cdaf7b7acb
KennyYi <kyoungjune.yi@gmail.com>
2025-08-26 22:06:39 +0900

    Add Button

--- src/page/SelectPage.tsx
+++ src/page/SelectPage.tsx
@@ -7,8 +7,10 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Dice4Icon, Dice5Icon, Dice6Icon } from "lucide-react";
 import React from "react";
 import { GenreEntry, GenreScriptFetcher } from "@/lib/GenreFetcher";
+import { Button } from "@/components/ui/button";
 
 const SelectPage: React.FC = () => {
   const [genres, setGenres] = useState<GenreEntry[]>([]);
@@ -25,26 +27,54 @@ const SelectPage: React.FC = () => {
 
   return (
     <div className="p-6 items-center justify-center flex flex-col">
-      <Select
-        value={selectedGenre?.genre ?? ""}
-        onValueChange={(value) => {
-          const selected = genres.find((genre) => genre.genre === value);
-          if (selected) setSelectedGenre(selected);
-        }}
-      >
-        <SelectTrigger className="w-[300px]">
-          <SelectValue placeholder="Select a music genre" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            {genres.map((genre) => (
-              <SelectItem key={genre.genre} value={genre.genre}>
-                {genre.genre}
-              </SelectItem>
-            ))}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
+      <div className="flex flex-row gap-4 w-full justify-items-end">
+        <div className="flex-1 justify-items-end">
+          <Select
+            value={selectedGenre?.genre ?? ""}
+            onValueChange={(value) => {
+              const selected = genres.find((genre) => genre.genre === value);
+              if (selected) setSelectedGenre(selected);
+            }}
+          >
+            <div className="flex flex-col gap-2 items-start">
+              <div className="text-lg text-gray-500">Select a genre</div>
+              <SelectTrigger className="w-[300px]">
+                <SelectValue placeholder="Select a music genre" />
+              </SelectTrigger>
+            </div>
+            <SelectContent>
+              <SelectGroup>
+                {genres.map((genre) => (
+                  <SelectItem key={genre.genre} value={genre.genre}>
+                    {genre.genre}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-1">
+          <div className="w-[1px] h-8 bg-gray-300" />
+          <div className="text-lg">or</div>
+          <div className="w-[1px] h-10 bg-gray-300" />
+        </div>
+        <div className="flex flex-col flex-1 gap-2 items-start justify-items-start">
+          <div className="flex flex-col flex-1 gap-2">
+            <div className="text-lg text-gray-500">Play random music</div>
+            <div className="w-inherit justify-items-center">
+              <Button
+                onClick={() => {
+                  console.log("Random music");
+                }}
+              >
+                <Dice6Icon />
+                <Dice4Icon />
+                <Dice5Icon />
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
